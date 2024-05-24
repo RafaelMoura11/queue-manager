aa32472208f98d9eaf6a780de3247def98877177
Deezinn <148241230+Deezinn@users.noreply.github.com>
2024-05-24 10:30:24 -0300

    criando a api

--- back-end/src/controllers/user-controller.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router()
-
-router.post('/users', async (req,res) =>{
-    return await res.status(201).json({
-        Message: 'Usuario cadastrado'
-    })
-})
