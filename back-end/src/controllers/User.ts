c564fec0a15cd4dfc569d798ec205ea7323fdea7
Rafael <rafaelmoura7033.com>
2024-05-28 17:39:32 -0300

    Creating User route

--- back-end/src/controllers/User.ts
+++ back-end/src/controllers/User.ts
@@ -1,10 +1,17 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import Login from '../interfaces/Login';
 import UserService from '../services/User';
+import RegisterInterface from '../interfaces/Register';
 
 export default class UserController {
   static async login(req: Request, res: Response): Promise<Response> {
     const token = UserService.login(req.body as Login);
     return res.status(200).json({ user: req.body.user, token });
+  }
+
+  static async register(req: Request, res: Response, next: NextFunction) {
+    const user = req.body;
+    UserService.register(user as RegisterInterface);
+    return res.status(200).json("Empregado registrado!");
   }
 }
--- back-end/src/services/User.ts
+++ back-end/src/services/User.ts
@@ -1,9 +1,29 @@
-import JWTUtils from 'src/utils/JWT';
+import UserModel from '../database/models/User';
+import JWTUtils from '../utils/JWT';
 import UserInterface from '../interfaces/Login';
+import RegisterInterface from '../interfaces/Register';
+import EmployeeService from './Employee';
+import * as bcrypt from 'bcryptjs';
 
 export default class User {
     static login(data: UserInterface): string {
         const token = JWTUtils.sign(data);
         return token;
+    }
+
+    static async register(data: RegisterInterface) {
+        await EmployeeService.create({
+            cpf: data.cpf,
+            fullName: data.fullName,
+            phone: data.phone
+        });
+        
+        await UserModel.create({
+            email: data.email,
+            password: bcrypt.hashSync(data.password),
+            employee_cpf: data.cpf
+        })
+
+        return true;
     }
 }
