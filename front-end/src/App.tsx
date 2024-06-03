443511ea5956f96a065726541e5bd626b3dee494
Rafael <rafaelmoura7033.com>
2024-06-03 09:45:49 -0300

    Moving some files to front-end foulder

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Provider from './context/Provider';
+import Menu from './pages/Menu';
 
 function App() {
   return (
     <BrowserRouter>
       <Provider>
         <Routes>
-          <Route path="/"></Route>
+          <Route path="/" element={ <Menu /> }></Route>
         </Routes>
       </Provider>
     </BrowserRouter>
